186e36cb526abef17edad31f400e98567b1a2870
llievre <loic.lievre@heig-vd.ch>
2019-06-13 08:44:22 +0200

    creation xsl, fenetre modale, pas de prise en compte des params pour l'instant

--- src/InterfaceRecherchePays.java
+++ src/InterfaceRecherchePays.java
@@ -6,9 +6,21 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class InterfaceRecherchePays extends JFrame {
     private JPanel panelRecherche = new JPanel(new FlowLayout());
-    private XMLParser parser = new XMLParser("countries.xml");
+    private XMLParser parser = new XMLParser("countries_NEW.xml");
     private JComboBox<String> continents = new JComboBox<>();
     private JComboBox<String> langages = new JComboBox<>();
     private JButton createXSL = new JButton("Générer XSL");
@@ -21,13 +33,184 @@ public class InterfaceRecherchePays extends JFrame {
 
             @Override
             public void mouseClicked(MouseEvent e) {
-
                 super.mouseClicked(e);
+                try {
+                    Document document = DocumentBuilderFactory
+                                        .newInstance()
+                                        .newDocumentBuilder()
+                                        .newDocument();
 
-                // Création des fichiers XSL selon ce qui est demandé
+                    Element root = document.createElement("xsl:stylesheet");
+                    root.setAttribute("version", "1.0");
+                    root.setAttribute("xmlns:xsl", "http://www.w3.org/1999/XSL/Transform");
 
-                /** A compléter... **/
+                    //OUTPUT
+                    Element output = document.createElement("xsl:output");
+                    output.setAttribute("method", "html");
+                    output.setAttribute("encoding", "UTF-8");
+                    output.setAttribute("doctype-public", "-//W3C//DTD HTML 4.01//EN");
+                    output.setAttribute("doctype-system", "http://www.w3.org/TR/html4/strict.dtd");
+                    output.setAttribute("indent", "yes");
 
+                    //TEMPLATE
+                    Element template = document.createElement("xsl:template");
+                    template.setAttribute("match", "/");
+                    Element html = document.createElement("html");
+
+                    //HEAD
+                    Element head = document.createElement("head");
+                    Element title = document.createElement("title");
+                    title.setTextContent("Liste des pays");
+                    Element jsJquery = document.createElement("script");
+                    jsJquery.setAttribute("src", "js/jquery-3.4.1.min.js");
+                    Element jsBootstrap = document.createElement("script");
+                    jsBootstrap.setAttribute("src", "js/bootstrap.min.js");
+                    Element cssBootstrap = document.createElement("link");
+                    cssBootstrap.setAttribute("rel", "stylesheet");
+                    cssBootstrap.setAttribute("href", "css/bootstrap.min.css");
+
+                    head.appendChild(title);
+                    head.appendChild(jsJquery);
+                    head.appendChild(jsBootstrap);
+                    head.appendChild(cssBootstrap);
+
+                    //BODY
+                    //*NAME
+                    Element body = document.createElement("body");
+
+                    Element button = document.createElement("button");
+                    button.setAttribute("type", "button");
+                    button.setAttribute("class", "btn btn-default"); //style du bouton?
+                    button.setAttribute("data-toggle", "modal");
+                    Element buttonTarget = document.createElement("xsl:attribute");
+                    buttonTarget.setAttribute("name", "data-target");
+                    Element buttonID = document.createElement("xsl:value-of");
+                    buttonID.setAttribute("select", "concat('#',alpha3Code)");
+                    buttonTarget.appendChild(buttonID);
+                    button.appendChild(buttonTarget);
+
+                    Element divRows = document.createElement("div");
+                    divRows.setAttribute("class", "row");
+
+                    Element foreach = document.createElement("xsl:for-each");
+                    foreach.setAttribute("select", "countries/element");
+                    Element sort = document.createElement("xsl:sort");
+                    sort.setAttribute("select","translations/fr");
+
+                    foreach.appendChild(sort);
+
+                    Element divCountries = document.createElement("div");
+                    divCountries.setAttribute("class", "col-sm-3");
+
+                    Element valueName = document.createElement("xsl:value-of");
+                    valueName.setAttribute("select", "translations/fr");
+                    divCountries.appendChild(valueName);
+
+                    //*IMAGE
+                    Element image = document.createElement("img");
+                    image.setAttribute("width", "25");
+                    Element imgName = document.createElement("xsl:attribute");
+                    imgName.setAttribute("name", "src");
+                    Element imgSrc = document.createElement("xsl:value-of");
+                    imgSrc.setAttribute("select", "flag");
+
+                    imgName.appendChild(imgSrc);
+                    image.appendChild(imgName);
+                    divCountries.appendChild(image);
+                    button.appendChild(divCountries);
+
+                    //MODAL
+                    Element divModale = document.createElement("div");
+                    divModale.setAttribute("class", "modal fade");
+
+                    Element idModale = document.createElement("xsl:attribute");
+                    idModale.setAttribute("name", "id");
+                    Element valueIdModale = document.createElement("xsl:value-of");
+                    valueIdModale.setAttribute("select", "alpha3Code");
+                    idModale.appendChild(valueIdModale);
+                    divModale.appendChild(idModale);
+
+
+                    //divModale.setAttribute("id", "exampleModal");
+                    divModale.setAttribute("tabindex", "-1");
+                    divModale.setAttribute("role", "dialog");
+                    divModale.setAttribute("aria-labelledby", "exampleModalLabel");
+                    divModale.setAttribute("aria-hidden", "true");
+
+                    Element divModaleDialog = document.createElement("div");
+                    divModaleDialog.setAttribute("class", "modal-dialog");
+                    divModaleDialog.setAttribute("role", "document");
+
+                    Element divModaleContent = document.createElement("div");
+                    divModaleContent.setAttribute("class", "modal-content");
+
+                    Element divModaleHeader = document.createElement("div");
+                    divModaleHeader.setAttribute("class", "modal-header");
+                    Element titleModale = document.createElement("h5");
+                    titleModale.setAttribute("class", "modal-title");
+
+                    Element valueNameModale = document.createElement("xsl:value-of");
+                    valueNameModale.setAttribute("select", "translations/fr");
+                    titleModale.appendChild(valueNameModale);
+                    divModaleHeader.appendChild(titleModale);
+
+                    Element divModaleBody = document.createElement("div");
+                    divModaleBody.setAttribute("class", "modal-body");
+
+                    Element divModaleFooter = document.createElement("div");
+                    divModaleFooter.setAttribute("class", "modal-footer");
+                    Element buttonCloseModale = document.createElement("button");
+                    buttonCloseModale.setAttribute("type", "button");
+                    buttonCloseModale.setAttribute("class", "btn btn-primary");
+                    buttonCloseModale.setAttribute("data-dismiss", "modal");
+                    buttonCloseModale.setTextContent("Fermer");
+
+                    divModaleFooter.appendChild(buttonCloseModale);
+
+                    divModaleContent.appendChild(divModaleHeader);
+                    divModaleContent.appendChild(divModaleBody);
+                    divModaleContent.appendChild(divModaleFooter);
+                    divModaleDialog.appendChild(divModaleContent);
+                    divModale.appendChild(divModaleDialog);
+                    //
+
+                    foreach.appendChild(button);
+                    foreach.appendChild(divModale);
+
+                    divRows.appendChild(foreach);
+                    body.appendChild(divRows);
+
+                    html.appendChild(head);
+                    html.appendChild(body);
+
+                    template.appendChild(html);
+
+                    // Ecriture de la racine dans le document
+                    document.appendChild(root);
+                    root.appendChild(output);
+                    root.appendChild(template);
+
+                    // Ecriture dans le fichier XSL
+                    DOMSource source = new DOMSource(document);
+
+                    FileWriter writer = new FileWriter(new File("countries.xsl"));
+                    StreamResult result = new StreamResult(writer);
+
+                    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                    Transformer transformer = transformerFactory.newTransformer();
+                    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+                    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+                    transformer.transform(source, result);
+
+                } catch (ParserConfigurationException error) {
+                    error.printStackTrace();
+                } catch (IOException error) {
+                    error.printStackTrace();
+                } catch (TransformerConfigurationException error) {
+                    error.printStackTrace();
+                } catch (TransformerException error) {
+                    error.printStackTrace();
+                }
             }
 
         });
@@ -38,18 +221,19 @@ public class InterfaceRecherchePays extends JFrame {
         ArrayList<String> dataContinents
                 = parser.parse("//countries/element/region[not(. = ../following-sibling::element/region)]");
 
+        Collections.sort(dataContinents);//TRI possible via xpath?
+
         //fill list with continents
         for(String s : dataContinents) {
             continents.addItem(s);
         }
-        Collections.sort(dataContinents);
 
-        //fill list with languages
         ArrayList<String> dataLangage = parser.parse("//countries/element/languages/element/name[not(. = ../../../following-sibling::element/languages/element/name)]");
+        Collections.sort(dataLangage); //TRI possible via xpath?
+        //fill list with languages
         for(String s : dataLangage) {
             langages.addItem(s);
         }
-        Collections.sort(dataLangage);
 
         setLayout(new BorderLayout());
 
@@ -73,8 +257,6 @@ public class InterfaceRecherchePays extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setTitle("Interface de recherche de pays");
-
-
     }
 
     public static void main(String ... args) {
--- src/XMLParser.java
+++ src/XMLParser.java
@@ -2,12 +2,10 @@
 
 import org.jdom2.input.SAXBuilder;
 
-import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.filter.Filters;
-import org.jdom2.input.SAXBuilder;
 import org.jdom2.xpath.XPathExpression;
 import org.jdom2.xpath.XPathFactory;
 
@@ -16,36 +14,28 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import java.io.InputStream;
-
 public class XMLParser {
-
     private XPathFactory xpath;
     Document document;
 
-    XMLParser(String file)
-    {
+    XMLParser(String file) {
         // parse and load file into memory
         InputStream in = XMLParser.class.getResourceAsStream(file);
         SAXBuilder builder = new SAXBuilder();
 
-
-        {
-            try {
-                document = builder.build(in);
-            } catch (JDOMException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        try {
+            document = builder.build(in);
+        } catch (JDOMException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         // create xpath factory
         xpath = XPathFactory.instance();
     }
 
-    public ArrayList<String> parse(String expression)
-    {
+    public ArrayList<String> parse(String expression){
         ArrayList<String> result = new ArrayList<>();
         XPathExpression<Element> expr = xpath.compile(expression, Filters.element());
         List<Element> courses = expr.evaluate(document);
